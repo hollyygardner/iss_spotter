6d3c8366dcd9bd5d89743a31b66238ffc3a59c81
Holly Gardner <hollylizgardner@gmail.com>
2022-09-01 18:30:44 -0400

    Testing fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +7,12 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP(mockData.ip, (error, coords) => {
+  if (error) {
+    console.log('It didn\'t work! Failed to fetch coordinates. \n', error);
+    return;
+  }
+  console.log('It worked! Your coordinates are: ', coords);
 });
