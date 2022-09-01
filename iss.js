ad63b23e0b9619d918bd721bf9f8e00fe9227ef4
Holly Gardner <hollylizgardner@gmail.com>
2022-09-01 18:34:43 -0400

    Fetch ISS flyovers for given location

--- iss.js
+++ iss.js
@@ -45,3 +45,25 @@ const fetchCoordsByIP = function(ip, callback) {
 };
 
 module.exports = { fetchCoordsByIP };
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+// Don't need to export the other functions since we are not testing them right now.
+module.exports = { fetchISSFlyOverTimes };
