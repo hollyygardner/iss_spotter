0a71a95241a19a468572d6abe5ed607661635787
Holly Gardner <hollylizgardner@gmail.com>
2022-09-01 19:30:31 -0400

    ISS Spotter solution with promises

---
+++ index2.js
@@ -0,0 +1,16 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss_promised');
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+
+// see index.js for printPassTimes 
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
