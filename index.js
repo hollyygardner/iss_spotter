107988b1608c5448ed3144f332a7bcc441f5d435
Holly Gardner <hollylizgardner@gmail.com>
2022-09-01 18:35:25 -0400

    Testing fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -9,10 +9,22 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:' , ip);
 });
 
-fetchCoordsByIP(mockData.ip, (error, coords) => {
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   if (error) {
-    console.log('It didn\'t work! Failed to fetch coordinates. \n', error);
+    console.log("It didn't work!" , error);
     return;
   }
-  console.log('It worked! Your coordinates are: ', coords);
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
